4152ef310ee2730de4e06a349d31495403382194
hernad <hernad@bring.out.ba>
2017-04-12 07:43:46 +0200

    cache .m2

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -12,6 +12,7 @@ docker rm -f android-build-$ANDROID_PROJECT
 
 docker run -ti \
        	-v $(pwd)/dot.android:/root/.android \
+       	-v $(pwd)/dot.m2:/root/.m2 \
        	-v $(pwd)/build:/build \
        	-v $(pwd)/apk:/apk \
 	-v $(pwd)/build_apk.sh:/build_apk.sh \
