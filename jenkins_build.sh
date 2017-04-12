380819c5cba1ae5866fc2c94ca117a8a7adf8f55
hernad <hernad@bring.out.ba>
2017-04-12 08:19:21 +0200

    github user in apk path, container name

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -7,8 +7,9 @@ fi
 
 GITHUB_USER=${1:-hernad}
 ANDROID_PROJECT=H4-android
+CONTAINER_NAME=android-build-$GITHUB_USER-$ANDROID_PROJECT
 
-docker rm -f android-build-$ANDROID_PROJECT
+docker rm -f  $CONTAINER_NAME
 
 docker run -t \
        	-v $(pwd)/dot.android:/root/.android \
@@ -16,7 +17,7 @@ docker run -t \
        	-v $(pwd)/build:/build \
        	-v $(pwd)/apk:/apk \
 	-v $(pwd)/build_apk.sh:/build_apk.sh \
-       	--name android-dev android-dev /build_apk.sh $GITHUB_USER
+       	--name $CONTAINER_NAME android-dev android-dev /build_apk.sh $GITHUB_USER
 
 
 cp -av apk/*.apk .
