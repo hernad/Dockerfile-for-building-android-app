77ae54742d542f891f8ebec435a0eb05e099a7db
hernad <hernad@bring.out.ba>
2017-04-12 08:06:08 +0200

    build reusable by multiple github users

--- build_apk.sh
+++ build_apk.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 
-GITHUB_USER=hernad
+GITHUB_USER=${1:-hernad}
+
 ANDROID_PROJECT=H4-android
 APK_DIR=/build/$ANDROID_PROJECT/vector/build/outputs/apk
 
 
-cd /build
+mkdir /build/$GITHUB_USER
+cd /build/$GITHUB_USER
+
 git clone https://github.com/$GITHUB_USER/$ANDROID_PROJECT.git
 
 
 cd $ANDROID_PROJECT
-git checkout apps_modular -f
+git checkout bringout -f
 git pull
 git log -1
 
--- jenkins_build.sh
+++ jenkins_build.sh
@@ -5,19 +5,18 @@ then
   docker build -t android-dev .
 fi
 
-GITHUB_USER=hernad
+GITHUB_USER=${1:-hernad}
 ANDROID_PROJECT=H4-android
 
 docker rm -f android-build-$ANDROID_PROJECT
 
-docker run -ti \
+docker run -t \
        	-v $(pwd)/dot.android:/root/.android \
        	-v $(pwd)/dot.m2:/root/.m2 \
        	-v $(pwd)/build:/build \
        	-v $(pwd)/apk:/apk \
 	-v $(pwd)/build_apk.sh:/build_apk.sh \
-       	--name android-dev android-dev /build_apk.sh
-
+       	--name android-dev android-dev /build_apk.sh $GITHUB_USER
 
 
 cp -av apk/*.apk .
